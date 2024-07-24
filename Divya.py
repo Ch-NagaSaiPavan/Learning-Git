6dad8beb845aaaf9757457d24aa8ade77f211629
Ch-NagaSaiPavan <nagasaipavan581@gmail.com>
2024-07-24 16:59:25 +0530

    Love

---
+++ Divya.py
@@ -0,0 +1,9 @@
+print("Hi Mardhal
+      I an NagaSaiPavan
+      I am writing this letter to propose you
+      I like your Smile, Eyes, and, Everything
+      I hope you will take much care about me and my family
+      Finally,
+      I Love So Much for Ever and Ever
+                        Yours Lovingly,
+                        Pavan (BAVA)") 
